d0f78702c9b954a51e8f0a565f4785c5d55685f9
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-06-07 11:06:36 +1200

    Add files via upload

---
+++ conditionalstate.py
@@ -0,0 +1,10 @@
+#write a program to check whether the given number is positive?
+num = 9000
+if num > 0:
+    print(num, "is a positive number.")
+
+num = -10
+if num < 0:
+    print(num, "is a negative number.")
+
+    
