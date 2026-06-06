5132c3941135d1b6915d186c5b3d77f8c4553840
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-06-07 11:25:33 +1200

    Add files via upload

---
+++ greaterorless.py
@@ -0,0 +1,11 @@
+#Write a program to check whether the given number is greater than 15 or smaller than 15.
+
+i = int(input("enter a number: "))
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in if Block")
+else:
+    print("i is greater than 15")
+    print("i'm in else Block")
+print("i'm not in if and not else Block ")
+
