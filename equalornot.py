aed812dd38e9c63dc8e4fe924c9b38fb142a3876
sethdahanayake-tech <seth.dahanayake@gmail.com>
2026-06-07 11:32:29 +1200

    Add files via upload

---
+++ equalornot.py
@@ -0,0 +1,11 @@
+#Write a program to check whether the given number is even or odd?
+
+number = int(input("Enter Number to check"))
+print("Number to be checked :", number)
+
+if number%2==0 :
+    print("This is an even number")
+
+else:
+    print("This is an odd number")
+
